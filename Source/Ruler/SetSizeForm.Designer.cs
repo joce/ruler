f5587af6d6a62a2ab47d854e335d3bc27f36d008
joce <jocelynlegault@gmail.com>
2017-04-23 12:54:01 -0400

    Tweak size dialogue btn placement & border style
    Signed-off-by: joce <jocelynlegault@gmail.com>

--- Source/Ruler/SetSizeForm.Designer.cs
+++ Source/Ruler/SetSizeForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Ruler
+namespace Ruler
 {
 	partial class SetSizeForm
 	{
@@ -35,78 +35,79 @@
 			this.btnCancel = new System.Windows.Forms.Button();
 			this.btnOk = new System.Windows.Forms.Button();
 			this.SuspendLayout();
-			// 
+			//
 			// label1
-			// 
+			//
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(3, 12);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(38, 13);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Width:";
-			// 
+			//
 			// txtWidth
-			// 
+			//
 			this.txtWidth.Location = new System.Drawing.Point(47, 12);
 			this.txtWidth.Name = "txtWidth";
 			this.txtWidth.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
 			this.txtWidth.Size = new System.Drawing.Size(107, 20);
 			this.txtWidth.TabIndex = 1;
-			// 
+			//
 			// txtHeight
-			// 
+			//
 			this.txtHeight.Location = new System.Drawing.Point(47, 36);
 			this.txtHeight.Name = "txtHeight";
 			this.txtHeight.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
 			this.txtHeight.Size = new System.Drawing.Size(107, 20);
 			this.txtHeight.TabIndex = 3;
-			// 
+			//
 			// label2
-			// 
+			//
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(3, 36);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(41, 13);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "Height:";
-			// 
+			//
 			// btnCancel
-			// 
+			//
 			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.btnCancel.Location = new System.Drawing.Point(6, 62);
 			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(63, 23);
+			this.btnCancel.Size = new System.Drawing.Size(72, 23);
 			this.btnCancel.TabIndex = 4;
 			this.btnCancel.Text = "Cancel";
 			this.btnCancel.UseVisualStyleBackColor = true;
 			this.btnCancel.Click += new System.EventHandler(this.BtnCancelClick);
-			// 
+			//
 			// btnOk
-			// 
-			this.btnOk.Location = new System.Drawing.Point(79, 62);
+			//
+			this.btnOk.Location = new System.Drawing.Point(82, 62);
 			this.btnOk.Name = "btnOk";
-			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.Size = new System.Drawing.Size(72, 23);
 			this.btnOk.TabIndex = 5;
 			this.btnOk.Text = "OK";
 			this.btnOk.UseVisualStyleBackColor = true;
 			this.btnOk.Click += new System.EventHandler(this.BtnOkClick);
-			// 
-			// SizeSetForm
-			// 
+			//
+			// SetSizeForm
+			//
 			this.AcceptButton = this.btnOk;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.btnCancel;
-			this.ClientSize = new System.Drawing.Size(165, 93);
+			this.ClientSize = new System.Drawing.Size(161, 92);
 			this.Controls.Add(this.btnOk);
 			this.Controls.Add(this.btnCancel);
 			this.Controls.Add(this.txtHeight);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.txtWidth);
 			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
-			this.Name = "SizeSetForm";
+			this.Name = "SetSizeForm";
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
